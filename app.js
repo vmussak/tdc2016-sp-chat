4ca0e4ff549f25469e4e2c1aef70a898a8deb521
Vinicius Mussak <vinicius.mussak@outlook.com>
2016-07-03 16:11:29 -0300

    Chat funcionando

---
+++ app.js
@@ -0,0 +1,39 @@
+const app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+    console.log('Alguém se conectou!');
+
+    socket.emit('mensagem', {
+        usuario: 'Servidor',
+        mensagem: 'Olá, seja bem-vindo ao chat!'
+    });
+
+    socket.on('login', (usuario) => {
+        socket.usuario = usuario;
+        socket.broadcast.emit('mensagem', {
+            usuario: 'Servidor',
+            mensagem: usuario + " entrou na conversa..."
+        });
+    });
+
+    socket.on('mensagem', (msg) => {
+        socket.broadcast.emit('mensagem', {
+            usuario: socket.usuario,
+            mensagem: msg
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Alguém saiu :(');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server Up!');
+});
